17eabe3483f715683261f55db891308d50085c95
Yas-son <5trk@bk.ru>
2022-02-10 11:57:11 +0300

    Add logika

---
+++ rational_calc.py
@@ -0,0 +1,14 @@
+
+from fractions import Fraction
+
+def rational_sum(a, b):
+    return Fraction(a)+Fraction(b)
+    
+def rational_minus(a, b):
+    return Fraction(a)-Fraction(b)
+
+def rational_division(a, b):
+    return Fraction(a)/Fraction(b)
+
+def rational_multi(a,b):
+    return Fraction(a)*Fraction(b)
